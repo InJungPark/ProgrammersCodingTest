c02ab0832520e419a0cd5f08200fdccd9485b3b2
InJungPark <in2066@naver.com>
2023-09-02 18:43:56 +0900

    두 수의 차

---
+++ src/injung/minuse/main.java
@@ -0,0 +1,21 @@
+package injung.minuse;
+
+public class main {
+
+    public static void main(String[] args) {
+
+        /**
+         *  https://school.programmers.co.kr/learn/courses/30/lessons/120803
+         *  정수 num1과 num2가 주어질 때, num1에서 num2를 뺀 값을 return하도록 soltuion 함수를 완성해주세요.
+         * */
+
+        int num1 = 2;
+        int mum2 = 3;
+
+        int soltuion = num1 - mum2;
+
+        System.out.println(soltuion);
+
+    }
+
+}
