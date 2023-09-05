e12082d6907bcb929cef66582a47667fa36e5bf2
InJungPark <in2066@naver.com>
2023-09-06 02:23:09 +0900

    두 수의 합

---
+++ src/injung/sumNumbers/main.java
@@ -0,0 +1,25 @@
+package injung.sumNumbers;
+
+public class main {
+
+    /**
+     * https://school.programmers.co.kr/learn/courses/30/lessons/120802
+     * 정수 num1과 num2가 주어질 때, num1과 num2의 합을 return하도록 soltuion 함수를 완성해주세요.
+     **/
+
+    public int twoSum(int num1, int num2) {
+        int sum = num1 + num2;
+        return sum;
+    }
+
+    public static void main(String[] args) {
+
+        int num1 = 2;
+        int num2 = 3;
+        int sum = num1 + num2;
+
+        System.out.println(sum);
+
+    }
+
+}
