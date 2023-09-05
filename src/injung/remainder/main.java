5e64efc9415215dccea55bfb3e67ffe5e0c5b3bd
InJungPark <in2066@naver.com>
2023-09-06 02:29:06 +0900

    나머지 구하기

--- src/injung/remainder/main.java
+++ src/injung/remainder/main.java
@@ -7,7 +7,7 @@ public class main {
          * https://school.programmers.co.kr/learn/courses/30/lessons/120810
          * 정수 num1, num2가 매개변수로 주어질 때, num1를 num2로 나눈 나머지를 return 하도록 solution 함수를 완성해주세요.
          *
-         * % 는 나머지를 구하는거다!!!
+         * %는 나머지를 구하는거다!!!
          **/
 
         int num1 = 3;
