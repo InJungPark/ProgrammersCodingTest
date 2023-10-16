e66ea76bba51c342175d887f2c4c9a544fba1a86
InJungPark <in2066@naver.com>
2023-10-16 11:34:27 +0900

    주석에 문제 제목적음

--- src/injung/minuse/main.java
+++ src/injung/minuse/main.java
@@ -7,7 +7,8 @@ public class main {
         /**
          * 두 수의 차
          * https://school.programmers.co.kr/learn/courses/30/lessons/120803
-         * 정수 num1과 num2가 주어질 때, num1에서 num2를 뺀 값을 return하도록 soltuion 함수를 완성해주세요.
+         * 정수 num1과 num2가 주어질 때, num1에서 num2를 뺀 값을
+         * return하도록 soltuion 함수를 완성해주세요.
          **/
 
         int num1 = 2;
